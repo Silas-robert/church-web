e22cd0e58ee77f9b281b599f8f15961545f247c8
silas-robert <silasrobert782@gmail.com>
2025-11-11 18:12:23 +0800

    updated settings.py

--- my_web/settings.py
+++ my_web/settings.py
@@ -20,12 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6&!cck#qeiq&d#@j8k8=w_*h5a9n-)hg!e&n$*5c-rm6zcc4pp'
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
